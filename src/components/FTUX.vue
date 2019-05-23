5330c8eae1ca250b9ae99abd251609f8216e8666
Daniel Daza <daniel.daza@zemoga.com>
2019-05-22 23:38:39 -0500

    Create FTUX form

--- src/components/FTUX.vue
+++ src/components/FTUX.vue
@@ -4,9 +4,42 @@
       <div class="column">
         <p>Hi! before you start to use this app, we want to know your name and some information about you.</p>
       </div>
+      <div class='ui centered card'>
+        <div class='content'>
+          <div class='ui form'>
+            <div class='field'>
+              <label>Name</label>
+              <input v-model="nameText" type='text'>
+            </div>
+            <div class='field'>
+              <label>Nickname</label>
+              <input v-model="nicknameText" type='text'>
+            </div>
+            <div class='field'>
+              <label>Mail</label>
+              <input v-model="mailText" type='text'>
+            </div>
+            <div class='ui two button attached buttons'>
+              <button class='ui basic blue button' v-on:click="sendForm()">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    sendForm() {
+      this.$emit('complete-ftux', {
+        name: this.nameText,
+        nickname: this.nicknameText,
+        mail: this.mailText,
+      });
+    },
+  },
+};
 </script>
